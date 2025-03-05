febb5bdb9ef8c832ae69010991dc22ab65476115
SasiKDP <sasank9110@gmail.com>
2025-03-05 14:52:36 +0530

    updated the submitted in the status in the requirements when an submissions is done in dev.

--- src/main/java/com/profile/candidate/repository/CandidateRepository.java
+++ src/main/java/com/profile/candidate/repository/CandidateRepository.java
@@ -66,7 +66,7 @@ public interface CandidateRepository extends JpaRepository<CandidateDetails, Str
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE requirements_model_prod r SET r.status = 'Submitted' " +
+    @Query(value = "UPDATE requirements_model r SET r.status = 'Submitted' " +
             "WHERE r.job_id = :jobId AND EXISTS " +
             "(SELECT 1 FROM candidates c WHERE c.job_id = :jobId)", nativeQuery = true)
     void updateRequirementStatus(@Param("jobId") String jobId);
