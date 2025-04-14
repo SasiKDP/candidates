715d8d92aec176a709b339725ad6a15beb65fa1a
Sri Charani Gudipudi <charanigudipudi1024@gmail.com>
2025-04-14 14:57:03 +0530

    added count of each table data by one get call

--- src/main/java/com/profile/candidate/repository/PlacementRepository.java
+++ src/main/java/com/profile/candidate/repository/PlacementRepository.java
@@ -2,6 +2,7 @@ package com.profile.candidate.repository;
 
 import com.profile.candidate.model.PlacementDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +10,14 @@ public interface PlacementRepository extends JpaRepository<PlacementDetails, Str
 
     boolean existsByPhone(String phone);
     boolean existsByConsultantEmail(String consultantEmail);
+    @Query(value = "SELECT " +
+            "(SELECT COUNT(*) FROM requirements_model) AS requirementsCount, " +
+            "(SELECT COUNT(*) FROM candidates) AS candidatesCount, " +
+            "(SELECT COUNT(*) FROM bdm_client) AS clientsCount, " +
+            "(SELECT COUNT(*) FROM placements) AS placementsCount, " +
+            "(SELECT COUNT(*) FROM bench_details) AS benchCount, " +
+            "(SELECT COUNT(*) FROM user_details) AS usersCount, " +
+            "(SELECT COUNT(*) FROM candidates WHERE interview_date_time IS NOT NULL) AS interviewsCount",
+            nativeQuery = true)
+    Object getAllCounts();
 }
