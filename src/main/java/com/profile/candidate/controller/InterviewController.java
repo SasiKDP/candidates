db78188db990decb396e5b72ba40aec75837ca93
SasiKDP <sasank9110@gmail.com>
2025-05-01 18:36:37 +0530

    added cross origins in interviews and submissions.

--- src/main/java/com/profile/candidate/controller/InterviewController.java
+++ src/main/java/com/profile/candidate/controller/InterviewController.java
@@ -19,6 +19,12 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://35.188.150.92", "http://192.168.0.140:3000", "http://192.168.0.139:3000","https://mymulya.com", "http://localhost:3000","http://192.168.0.135:3000",
+        "http://192.168.0.135:80",
+        "http://mymulya.com:443",
+        "http://182.18.177.16:443",
+        "http://localhost/","http://192.168.0.135",
+        "http://182.18.177.16"})
 @RestController
 @RequestMapping("/candidate")
 public class InterviewController {
